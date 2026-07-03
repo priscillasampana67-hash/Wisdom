c98586f721da342a84e6e528be1c11c10ba5ca63
Priscilla Sampana <priscillasampana67@gmail.com>
2026-07-03 21:07:45 +0000

    snake

---
+++ snake.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+bool gameOver;
+const int width = 20, height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw() {
+    system("cls");
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+
+            if (j == 0)
+                cout << "#";
+
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "*";
+            else
+                cout << " ";
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+
+    cout << "\nScore: " << score << endl;
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'a': dir = LEFT; break;
+        case 'd': dir = RIGHT; break;
+        case 'w': dir = UP; break;
+        case 's': dir = DOWN; break;
+        case 'x': gameOver = true; break;
+        }
+    }
+}
+
+void Logic() {
+    switch (dir) {
+    case LEFT: x--; break;
+    case RIGHT: x++; break;
+    case UP: y--; break;
+    case DOWN: y++; break;
+    }
+
+    if (x >= width) x = 0;
+    else if (x < 0) x = width - 1;
+
+    if (y >= height) y = 0;
+    else if (y < 0) y = height - 1;
+
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+    }
+}
+
+int main() {
+    Setup();
+
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        Sleep(100);
+    }
+
+    return 0;
+}
