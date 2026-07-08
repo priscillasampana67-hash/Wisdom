59e05109d2145042b38b36f4e6c00648e1a5019d
Priscilla Sampana <priscillasampana67@gmail.com>
2026-07-08 22:16:37 +0000

    Area

---
+++ area.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float side, area;
+
+    cout << "Enter the length of the side of the square: ";
+    cin >> side;
+
+    area = side * side;
+
+    cout << "Area of the square = " << area << endl;
+
+    return 0;
+}
