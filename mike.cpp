0b7ac09a3f49977f4ae0c22107a7913671e8f6e9
Priscilla Sampana <priscillasampana67@gmail.com>
2026-08-01 22:56:54 +0000

    Mike

---
+++ mike.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+// Abstract Base Class
+class Shape
+{
+public:
+    virtual double area() = 0;   // Pure virtual function
+};
+
+// Derived Class: Rectangle
+class Rectangle : public Shape
+{
+private:
+    double length, width;
+
+public:
+    Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+
+    double area() override
+    {
+        return length * width;
+    }
+};
+
+// Derived Class: Circle
+class Circle : public Shape
+{
+private:
+    double radius;
+
+public:
+    Circle(double r)
+    {
+        radius = r;
+    }
+
+    double area() override
+    {
+        return 3.14159 * radius * radius;
+    }
+};
+
+int main()
+{
+    Rectangle rect(10, 5);
+    Circle circ(7);
+
+    Shape *shape;   // Shape pointer
+
+    // Point to Rectangle object
+    shape = &rect;
+    cout << "Rectangle Area = " << shape->area() << endl;
+
+    // Point to Circle object
+    shape = &circ;
+    cout << "Circle Area = " << shape->area() << endl;
+
+    return 0;
+}
+
