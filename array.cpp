951cf7b90c6b00c2d5fb01368d5dc2a9a42acdc7
Priscilla Sampana <priscillasampana67@gmail.com>
2026-08-02 23:14:18 +0000

    Initial Flutter heart app

---
+++ array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int score[5] = {90, 85, 77, 92, 88};
+    for(int i=0; i < 5;i++) {
+        cout << score [i] << " \n "; 
+} 
+    
+
+} 
