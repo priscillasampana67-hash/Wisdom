7328b56cd74c07ae8dec1a541116d81a29af6adb
Priscilla Sampana <priscillasampana67@gmail.com>
2026-09-09 23:34:25 +0000

    Py

---
+++ angie.py
@@ -0,0 +1,15 @@
+#x, y, z ="Orange", "Banana", "Cherry"
+#print(x)
+#print(y)
+#print(z)
+
+#a = b = c = "Mango"
+#print(a)
+#print(b)
+#print(c)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
