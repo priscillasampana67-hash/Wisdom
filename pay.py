838f5e8d8a97d4b85746caa03fcf2f779012b07e
Priscilla Sampana <priscillasampana67@gmail.com>
2026-09-18 17:02:08 +0000

    Patience

---
+++ pay.py
@@ -0,0 +1,60 @@
+#x = 1
+#y = 2.8
+#z = 1j
+
+"""
+a = float(x)
+b = int(y)
+c = complex(y)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+"""
+
+#import random
+#print(random.randrange(1,10))
+"""
+a = "Hello, world!"
+print(a[4])
+
+for x in "banana":
+    print(x)
+
+a = "Hello, world!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("hello" in txt)
+print("best" in txt)
+
+if "free" in txt:
+    print("Yes, 'free' is presnt.")
+
+    print("expensive" not in txt)
+
+    b = "Hello, world!"
+    print(b[-5:-2])
+
+    a = " Hello, world! "
+    print(a.upper())
+    print(a.lower())
+    print(a.strip())
+    print(a.replace("H", "J"))
+    print(a.split(","))
+    """
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20*59} dollars"
+print(txt)
